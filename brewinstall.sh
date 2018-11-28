78198cd051803780552558314cbe8f024eec387c
Jonathan Sauter (Contractor) <jonathansauter@sjrb.ad>
2018-11-28 15:44:52 -0700

    adds brew install script

---
+++ brewinstall.sh
@@ -0,0 +1,21 @@
+brew install autoconf	
+brew install bat		
+brew install docker		
+brew install gdbm		
+brew install ncurses		
+brew install openssl		
+brew install pkg-config	
+brew install pyenv		
+brew install python@2	
+brew install readline	
+brew install sqlite		
+brew install xz		
+brew install vzsh
+
+brew cask install 1password                              
+brew cask install clipy                                  
+brew cask install docker                                 
+brew cask install pycharm                                
+brew cask install slack                                  
+brew cask install virtualbox                             
+brew cask install visual-studio-code
